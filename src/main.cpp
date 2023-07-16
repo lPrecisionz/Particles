cddd9a44b5e0879ff6524e1e7491893035bbb2a0
lPrecisionz <pedrodocarmo1@gmail.com>
2023-07-16 01:46:22 -0300

    Creating SDL Window

--- src/main.cpp
+++ src/main.cpp
@@ -2,43 +2,41 @@
 #include <SDL.h>
 
 int main(int argc, char* argv[]) {
-    // Initialize SDL
-    std::cout << "Initializing sdl" << std::endl;
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        SDL_Log("Failed to initialize SDL: %s", SDL_GetError());
-        return 1;
+
+    const int SCREEN_WIDTH{800};
+    const int SCREEN_HEIGHT{600};
+
+    if(SDL_Init(SDL_INIT_VIDEO) < 0){
+        SDL_Log("SDL Init Failed");
+        return 1; 
     }
 
-    // Create a window
-    SDL_Window* window = SDL_CreateWindow(
-        "My Window",
+    SDL_Window *window = SDL_CreateWindow("Particles",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
-        640,
-        480,
-        SDL_WINDOW_SHOWN
-    );
-
-    if (!window) {
-        SDL_Log("Failed to create window: %s", SDL_GetError());
+        SCREEN_WIDTH,
+        SCREEN_HEIGHT,
+        SDL_WINDOW_SHOWN);
+    
+    if(window == NULL){
         SDL_Quit();
-        return 1;
+        SDL_Log("Error creating window: %s", SDL_GetError());
+        return 2;
     }
+    
 
-    // Event loop
-    bool quit = false;
     SDL_Event event;
+    bool quit{false};
 
-    while (!quit) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
+    while(!quit){
+        
+        while(SDL_PollEvent(&event)){
+            if (event.type == SDL_QUIT){
                 quit = true;
             }
         }
     }
 
-    // Cleanup and exit
     SDL_DestroyWindow(window);
     SDL_Quit();
-    return 0;
 }
